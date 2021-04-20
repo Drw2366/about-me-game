6369f3a5481dfa14d08d7e00350cebc21f4d9cd1
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-04-20 12:50:34 -0400

    Revamoed the questions

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ alert(response1 + theirName);
 
 // If conditional based confirm command. The 5 questions
 
-let answer1 = confirm('Are you from Georgia?');
-if (answer1 === true) {
+// Question 1
+let answer1 = prompt('Are you from Georgia?');
+if (answer1 === 'yes') {
 //   console.log('Really, so am I.');
   alert('Really, so am I.');
 } else {
@@ -17,8 +18,9 @@ if (answer1 === true) {
 }
 
 
-let cityState = confirm('Would you ever come to Atlanta, Ga?');
-if (cityState === true) {
+// Question 2
+let cityState = prompt('Would you ever come to Atlanta, Ga?');
+if (cityState === 'yes') {
 //   console.log('Your making a grand gesture.');
   alert('Your making a grand gesture.');
 } else {
@@ -26,8 +28,10 @@ if (cityState === true) {
   alert('Well stay where you at lol!!!!');
 }
 
-let blackUniversity = confirm('Do you know any of the HBCU\'s in Atlanta, Ga?');
-if (blackUniversity === true) {
+
+// Question 3
+let blackUniversity = prompt('Do you know any of the HBCU\'s in Atlanta, Ga?');
+if (blackUniversity === 'yes') {
 //   console.log('There are 10 HBCU\'s in Atanta, Ga');
   alert('There are 10 HBCU\'s in Atlanta, Ga');
 } else {
@@ -35,8 +39,10 @@ if (blackUniversity === true) {
   alert('You should definitely visit at least 5 HBU\'s when you come and visit.');
 }
 
-let facts = confirm('Do you know how many black-owned bussines there are in Georgia');
-if (facts === true) {
+
+// Question 4
+let facts = prompt('Do you know how many black-owned bussines there are in Georgia');
+if (facts === 'yes') {
 //   console.log('I didn\'t know that');
   alert('I didn\'t know that.');
 } else {
@@ -44,8 +50,9 @@ if (facts === true) {
   alert('There are about 8,900 black-owned businesses in the state, that employ just under 83,800 people.');
 }
 
-let tylerPerry = confirm('Did you know that Steve Harvey bought Tyler Perry\'s house in Atlanta.');
-if(tylerPerry === true) {
+// Question 5
+let tylerPerry = prompt('Did you know that Steve Harvey bought Tyler Perry\'s house in Atlanta.');
+if(tylerPerry === 'yes') {
 //   console.log('Its 35,000 sqft and cost 15 million dollars.');
   alert('It\'s 35,000 sqft and cost $15 million dollars');
 } else {
@@ -55,18 +62,26 @@ if(tylerPerry === true) {
 
 // TolowerCase or ToUppercase
 
-let car = prompt('What is your dream car?');
-
-switch (car.toLowerCase()) {
-case 'Rolls Royce':
-  console.log('Your favorite car was a Rolls Royce');
-  break;
-case 'Range Rover':
-  console.log('Your favorite car was Range Rover');
-  break;
-default:
-  console.log('I now know what is your dream car.');
+let car = prompt('What is my dream car? Hint The initails are RR. ');
+if(car.toLowerCase() === 'rolls royce'){
+  alert('Wow how did you know that?');
+  //  console.log('Wow how did you know that?');
+} else {
+  alert('Nope! The right answer is Rolls Royce.');
+  // Console.log('Nope! The right answer is Rolls Royce');
 }
+
+
+// // switch (car.toLowerCase()) {
+// // case 'Rolls Royce':
+// //   console.log('Your favorite car was a Rolls Royce');
+// //   break;
+// // case 'Range Rover':
+// //   console.log('Your favorite car was Range Rover');
+// //   break;
+// // default:
+// //   console.log('I now know what is your dream car.');
+// // }
 
 // Question 6
 alert('Guess a number between 1-11. You will have 4 attempts!');
@@ -92,34 +107,35 @@ while(i < 3 && guess !== answer){
 
 alert(`The answer is ${answer}`);
 
-let car3 = ['Roll Royce', 'Range Rover', 'Porche', 'Bentley', 'Audi', 'Maybach'];
-let rightAnswer = 'Rolls Royce';
+
+let car3 = ['Rolls Royce', 'Range Rover', 'Porche', 'Bentley', 'Audi', 'Maybach'];
+
 let answer3 = prompt('What is the users dream car?' + car3);
 let x = 0;
 let userPoints = 0;
 
 while( x < 7){
-  if(answer3.toLowerCase !== rightAnswer){
-    alert('nope');
-    answer3 = prompt('What is the users dream car?');
-  } else {
+  if(answer3.toLowerCase === 'rolls royce'){
     alert('Thats Correct!!!');
     alert(`You have ${userPoints} left`);
+    x++;
+  } else {
+    alert('nope');
     userPoints++;
   }
-  x++;
+
 }
 
-while(x < 7){
-  for (let i = 0; i < ingredientsToPizza.length; i++) {
-    // alert(ingredientsToPizza[i]);
-    if(answer3 === ingredientsToPizza[i]){
-      // If we find the answer, it true.
-      alert(answer3);
-      alert(ingredientsToPizza[i]);
-      x = 7;
-    }
-  }
-  answer3 = prompt('What are the main ingredients to make a pizza?');
-  x++;
-}
+// while(x < 7){
+//   for (let i = 0; i < ingredientsToPizza.length; i++) {
+//     // alert(ingredientsToPizza[i]);
+//     if(answer3 === ingredientsToPizza[i]){
+//       // If we find the answer, it true.
+//       alert(answer3);
+//       alert(ingredientsToPizza[i]);
+//       x = 7;
+//     }
+//   }
+//   answer3 = prompt('What are the main ingredients to make a pizza?');
+//   x++;
+// }
